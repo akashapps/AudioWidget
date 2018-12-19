9929e9f810df13cf810ce72950c6004a64062116
Akash <akash.abap@yahoo.com>
2018-12-19 17:39:23 -0500

    Continue

--- app/src/main/java/com/akash/audiowidget/views/VolumeControllerRemoteViews.java
+++ app/src/main/java/com/akash/audiowidget/views/VolumeControllerRemoteViews.java
@@ -4,13 +4,16 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.os.Bundle;
 import android.widget.RemoteViews;
 
-import com.akash.audiowidget.ListViewAudioWidget;
+import com.akash.audiowidget.AudioWidget;
 import com.akash.audiowidget.R;
 import com.akash.audiowidget.models.AudioStreamType;
 
 public class VolumeControllerRemoteViews extends RemoteViews {
+
+    public static String EXTRA_DATA = "EXTRA_DATA";
 
     public VolumeControllerRemoteViews(Context context) {
         this(context.getPackageName(), R.layout.list_item_voulume_cell);
@@ -24,14 +27,16 @@ public class VolumeControllerRemoteViews extends RemoteViews {
 
         setTextViewText(R.id.title, type.title);
         setProgressBar(R.id.progress_bar, type.getMaxVolume(audioManager), type.getVolume(audioManager), false);
-        setOnClickPendingIntent(R.id.plus, getPendingSelfIntent(context, type.volumeUpString));
-        setOnClickPendingIntent(R.id.minus, getPendingSelfIntent(context, type.volumeDownString));
+
+        setOnClickFillInIntent(R.id.plus, getIntent(type.volumeUpString));
+        setOnClickFillInIntent(R.id.minus, getIntent(type.volumeDownString));
     }
 
-    protected PendingIntent getPendingSelfIntent(Context context, String action) {
-        Intent intent = new Intent(context, ListViewAudioWidget.class);
-//        Intent intent = new Intent(context, getClass());
-        intent.setAction(action);
-        return PendingIntent.getBroadcast(context, 0, intent, 0);
+    private Intent getIntent(String action) {
+        Intent intent = new Intent();
+        Bundle extras = new Bundle();
+        extras.putString(EXTRA_DATA, action);
+        intent.putExtras(extras);
+        return intent;
     }
 }
